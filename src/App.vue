4966fc3627966c35a06436d6ebe0ccffc1fa467f
Abousaid Salah-Eddine <abousaid7salah@gmail.com>
2019-03-28 12:25:04 +0100

    work with API, and Router

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,16 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos  v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Header from './components/layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-
+import Header from './components/layout/Header'
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    Header,
-    Todos,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
-      ]
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id != id);
-    },
-    addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
-      this.title = '';
-    }
+    Header
   }
 }
 </script>
